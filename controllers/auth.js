68b592aa7413f19755390f120bfceb910aadc1a7
papapin <papapin777#gmail.com>
2018-08-16 22:41:12 -0700

    b3-l3

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 
 const User = require('../model/user');
 
@@ -10,15 +11,33 @@ module.exports.login = function( req, res ){
      });
 }
 
-module.exports.register = function (req, res) {
+module.exports.register = async function (req, res) {
 
-   const user = new User({
-     email: req.body.email,
-     password: req.body.password
-   });
+    const candidate = await User.findOne({email: req.body.email});
 
-    user.save().then(()=>{
-        console.log('user created');
-    });
+    if (candidate) {
+        res.status(409).json({
+            message: 'Email already taken'
+        })
+    } else {
+
+                const salt = bcrypt.genSaltSync(10);
+                const password = req.body.password;
+
+                const user = new User({
+                  email: req.body.email,
+                  password: bcrypt.hashSync(password, salt)
+                });
+
+                try {
+                    await user.save();
+                    res.status(201).json({ user })
+                } catch(e) {
+                    //error
+                }
+                
+           }
+
+
 
 }
