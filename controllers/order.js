2b81cb83cbe907e66068314aaeaf91f70cdea295
papapin <papapin777#gmail.com>
2018-10-21 12:54:27 -0700

    b4-l6  api is done( without analitics ctrl  ) / 29

--- controllers/order.js
+++ controllers/order.js
@@ -1,8 +1,62 @@
-module.exports.getAll = function(req, res) {
+const Order = require('../model/order');
+const errorHandler = require("../utils/errorHendler");
+
+module.exports.getAll = async function(req, res) {
+
+    const query = {
+        user: req.user.id,
+
+    }
+
+    if(req.query.start) {
+        query.date = { $gte: req.query.start };
+    }
+
+    if (req.query.end) {
+        query.date = { $gte: req.query.start };
+        if (!query.date) {
+            query.date = {}
+        }
+
+        query.date["$lte"] = req.query.end;
+    }
+
+    if (req.query.order) {
+        query.order = +req.query.order;
+    }
+
+    try {
+        
+        const order = await Order.find(query).sort({ date: -1 }).skip(+req.query.offset).limit(+req.query.limit)
+        
+        res.status(200).json( );
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
 
 };
 
 
-module.exports.create = function (req, res) {
+module.exports.create = async function (req, res) {
+
+    const lastOrder = await Order.findOne({user: req.user.id}).sort({date: -1});
+
+    const maxOrder = lastOrder ? lastOrder.order : 0;
+
+    try {
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order: maxOrder + 1,
+
+        }).save();
+
+        
+        res.status(201).json({order});
+
+    } catch (e) {
+        errorHandler(res, e)
+    }
 
 };
